06cdab45c09c81f30cd1def026184c9d7543bf78
Hariharan <hariharan80122@gmail.com>
2021-08-21 18:20:03 +0530

    update Pattern

--- DesignPatterns/BehavioralPattern/StrategyPattern/ItemCost.java
+++ DesignPatterns/BehavioralPattern/StrategyPattern/ItemCost.java
@@ -1,3 +1,163 @@
-public class ItemCost {
+import java.util.Scanner;
 
+public class ItemCost {
+    int price;
+    int itemId;
+    int option;
+    String itemModelName;
+    Scanner sc = new Scanner(System.in);
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemModelName() {
+        return itemModelName;
+    }
+
+    public ItemCost(String itemName) {
+        if (itemName.equalsIgnoreCase("Mobile")) {
+            mobileModel();
+        } else if (itemName.equalsIgnoreCase("Smart Watch")) {
+            smartWatchModel();
+        } else if (itemName.equalsIgnoreCase("Laptop")) {
+            laptopModel();
+        } else if (itemName.equalsIgnoreCase("Hard Drive")) {
+            hardDriveModel();
+        }
+    }
+
+    private void hardDriveModel() {
+        System.out.println("Hard Drive!!!!");
+        System.out.println("============================ ");
+        System.out.println("1. Sony 1 TB : Rs.4000 ");
+        System.out.println("2. Samsung 1 TB : Rs.3500 ");
+        System.out.println("3. Green 2 TB : Rs.4500");
+        System.out.println("============================ ");
+        option = sc.nextInt();
+        sc.nextLine();
+        switch (option) {
+            case 1:
+                this.itemId = 1401;
+                this.price = 4500;
+                this.itemModelName = "Sony 1 TB";
+                break;
+            case 2:
+                this.itemId = 1402;
+                this.price = 3500;
+                this.itemModelName = "Samsung 1 TB";
+                break;
+            case 3:
+                this.itemId = 1403;
+                this.price = 4500;
+                this.itemModelName = "Green 2 TB";
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void laptopModel() {
+        System.out.println("Laptop model!!!!");
+        System.out.println("============================ ");
+        System.out.println("1. Acer : Rs.45,000");
+        System.out.println("2. Hp : Rs.72,000");
+        System.out.println("3. Lenovo : Rs.49,000");
+        System.out.println("4. Apple : Rs.1,30,000");
+        System.out.println("============================ ");
+        option = sc.nextInt();
+        sc.nextLine();
+        switch (option) {
+            case 1:
+                this.itemId = 1101;
+                this.price = 45000;
+                this.itemModelName = "Acer Laptop";
+                break;
+            case 2:
+                this.itemId = 1102;
+                this.price = 72000;
+                this.itemModelName = "Hp Laptop";
+                break;
+            case 3:
+                this.itemId = 1103;
+                this.price = 49000;
+                this.itemModelName = "Lenovo Laptop";
+                break;
+            case 4:
+                this.itemId = 1104;
+                this.price = 130000;
+                this.itemModelName = "Apple Laptop";
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void smartWatchModel() {
+        System.out.println("Welcome to shopping cart!!!!");
+        System.out.println("============================ ");
+        System.out.println("1. Mi Smart Watch : Rs.6000");
+        System.out.println("2. Apple Watch : Rs.30,000");
+        System.out.println("3. Samsung : Rs.15,000");
+        System.out.println("============================ ");
+        option = sc.nextInt();
+        sc.nextLine();
+        switch (option) {
+            case 1:
+                this.itemId = 1201;
+                this.price = 6000;
+                this.itemModelName = "Mi Smart Watch";
+                break;
+            case 2:
+                this.itemId = 1202;
+                this.price = 30000;
+                this.itemModelName = "Apple Watch";
+                break;
+            case 3:
+                this.itemId = 1203;
+                this.price = 15000;
+                this.itemModelName = "Samsung";
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void mobileModel() {
+        System.out.println("Welcome to shopping cart!!!!");
+        System.out.println("============================ ");
+        System.out.println("1. Mi Mobile : Rs.15,000 ");
+        System.out.println("2. Samsung Mobile : Rs.25,000 ");
+        System.out.println("3. Vivo Mobile : Rs.16,000");
+        System.out.println("4. Apple Mobile : Rs.86,000 ");
+        System.out.println("============================ ");
+        option = sc.nextInt();
+        sc.nextLine();
+        switch (option) {
+            case 1:
+                this.itemId = 1301;
+                this.price = 15000;
+                this.itemModelName = "Mi Mobile";
+                break;
+            case 2:
+                this.itemId = 1302;
+                this.price = 25000;
+                this.itemModelName = "Samsung Mobile";
+                break;
+            case 3:
+                this.itemId = 1303;
+                this.price = 16000;
+                this.itemModelName = "Vivo Mobile";
+                break;
+            case 4:
+                this.itemId = 1304;
+                this.price = 86000;
+                this.itemModelName = "Apple Mobile";
+                break;
+        }
+    }
 }
--- DesignPatterns/BehavioralPattern/StrategyPattern/StrategyPatternExample.java
+++ DesignPatterns/BehavioralPattern/StrategyPattern/StrategyPatternExample.java
@@ -10,8 +10,9 @@ public class StrategyPatternExample {
         ShoppingCart cart = new ShoppingCart();
         Scanner sc = new Scanner(System.in);
         Boolean bool = true;
+        int option;
 
-        while (bool){
+        while (bool) {
             System.out.println("Welcome to shopping cart!!!!");
             System.out.println("============================ ");
             System.out.println("1. Mobile ");
@@ -19,32 +20,71 @@ public class StrategyPatternExample {
             System.out.println("3. Laptop ");
             System.out.println("4. Hard Drive ");
             System.out.println("============================ ");
-            int option = sc.nextInt();
+            option = sc.nextInt();
             sc.nextLine();
-            switch (option){
+            ItemCost itemCost;
+            Item item = null;
+            switch (option) {
                 case 1:
-
+                    itemCost = new ItemCost("Mobile");
+                    item = new Item(String.valueOf(itemCost.getItemId()), itemCost.getItemModelName(), itemCost.getPrice());
                     break;
                 case 2:
+                    itemCost = new ItemCost("Smart Watch");
+                    item = new Item(String.valueOf(itemCost.getItemId()), itemCost.getItemModelName(), itemCost.getPrice());
+
                     break;
                 case 3:
+                    itemCost = new ItemCost("Laptop");
+                    item = new Item(String.valueOf(itemCost.getItemId()), itemCost.getItemModelName(), itemCost.getPrice());
+
                     break;
                 case 4:
+                    itemCost = new ItemCost("Hard Drive");
+                    item = new Item(String.valueOf(itemCost.getItemId()), itemCost.getItemModelName(), itemCost.getPrice());
                     break;
                 default:
+                    System.out.println("choose correct");
                     break;
             }
+            cart.addItem(item);
+            System.out.println("Do you want to continue enter true/false");
+            bool = sc.nextBoolean();
         }
-        Item item1 = new Item("1002","mobile",25000);
-        Item item2 = new Item("5678","smart watch",15000);
+//        Item item1 = new Item("1002", "mobile", 25000);
+//        Item item2 = new Item("5678", "smart watch", 15000);
 
-        cart.addItem(item1);
-        cart.addItem(item2);
 
-        //pay by paypal
-        cart.pay(new PaypalStrategy("hariharan80122@gmail.com", "ED80122"));
+        System.out.println("Which option you want to pay!!!");
+        System.out.println("============================ ");
+        System.out.println("1. PayPal ");
+        System.out.println("2. CreditCard ");
+        System.out.println("============================ ");
+        option = sc.nextInt();
+        sc.nextLine();
+        switch (option){
+            case 1:
+                System.out.println("Enter mail id : ");
+                String mailId = sc.nextLine();
+                System.out.println("Enter password : ");
+                String password = sc.nextLine();
 
-        //pay by credit card
-        cart.pay(new CreditCardStrategy("Hariharan", "80122544458889", "246", "11/25"));
+                cart.pay(new PaypalStrategy(mailId, password));
+                break;
+            case 2:
+                System.out.println("Enter Name : ");
+                String name = sc.nextLine();
+                System.out.println("Enter Credit card : ");
+                String ccNum = sc.nextLine();
+                System.out.println("Enter cvv no : ");
+                String cvv = sc.nextLine();
+                System.out.println("Enter expire date : ");
+                String expireDate = sc.nextLine();
+
+                cart.pay(new CreditCardStrategy(name, ccNum, cvv, expireDate));
+                break;
+        }
+
+
     }
 }
